01a011e23571b08dea50b7c6569593ef86573866
Егор Пигалёв <1pigaleve@mail.ru>
2023-03-04 13:09:39 +0300

    Добавьте файлы проекта.

---
+++ Pigalev_Sessia1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Pigalev_Sessia1.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pigalev_Sessia1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Base.baseDate = new BaseDate();
+            FrameClass.frame = mainFrame;
+            FrameClass.frame.Navigate(new Login());
+        }
+    }
+}
---
+++ Pigalev_Sessia1/pages/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Pigalev_Sessia1.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pigalev_Sessia1
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAutorization_Click(object sender, RoutedEventArgs e)
+        {
+            User user = Base.baseDate.User.FirstOrDefault(x => x.UserLogin == tbLogin.Text && x.UserPassword == pbPassword.Password);
+            if(user == null)
+            {
+                CAPTCHA captcha = new CAPTCHA(user);
+                captcha.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Пользователь с таким логиным и паролем не найден!");
+            }
+        }
+    }
+}
